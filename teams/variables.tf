c6cdeb03bc807a07aa7ce8376ef0eda303d52b3e
mattv1vhm <130549829+mattv1vhm@users.noreply.github.com>
2025-08-09 17:11:33 +0100

    feat: add team Terraform module

--- teams/variables.tf
+++ teams/variables.tf
@@ -1,2 +1,27 @@
-// Input variables for team module
+variable "name" {
+  description = "Name of the team"
+  type        = string
+}
 
+variable "description" {
+  description = "Team description"
+  type        = string
+  default     = ""
+}
+
+variable "privacy" {
+  description = "Team privacy setting"
+  type        = string
+  default     = "closed"
+
+  validation {
+    condition     = contains(["secret", "closed"], var.privacy)
+    error_message = "Privacy must be 'secret' or 'closed'."
+  }
+}
+
+variable "members" {
+  description = "List of usernames to add to the team"
+  type        = list(string)
+  default     = []
+}

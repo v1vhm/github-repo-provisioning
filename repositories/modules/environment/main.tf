f599a765c3cb820c7c6f4a39e06f1909688a01fe
mattv1vhm <130549829+mattv1vhm@users.noreply.github.com>
2025-08-22 19:29:25 +0100

    Create plan and apply environments

--- repositories/modules/environment/main.tf
+++ repositories/modules/environment/main.tf
@@ -11,14 +11,20 @@ provider "github" {
   owner = var.owner
 }
 
+locals {
+  phases = ["plan", "apply"]
+}
+
 resource "github_repository_environment" "this" {
+  for_each    = toset(local.phases)
   repository  = var.repository
-  environment = var.environment_name
+  environment = "${var.environment_name}-${each.key}"
 }
 
 resource "github_actions_environment_secret" "azure_client_id" {
+  for_each        = github_repository_environment.this
   repository      = var.repository
-  environment     = github_repository_environment.this.environment
+  environment     = each.value.environment
   secret_name     = "AZURE_CLIENT_ID"
   plaintext_value = var.managed_identity_client_id
 }
